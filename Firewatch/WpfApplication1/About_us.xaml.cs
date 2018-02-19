88a596610782c09d7b0636269aa813a8e9f6f3a3
Ekaterenium <yaroslavnakate@gmail.com>
2018-02-19 19:34:20 +0500

    Катя)

---
+++ Firewatch/WpfApplication1/About_us.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для About_us.xaml
+    /// </summary>
+    public partial class About_us : Window
+    {
+        public About_us()
+        {
+            InitializeComponent();
+        }
+
+        private void На_главную_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow form = new MainWindow();
+            form.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Firewatch/WpfApplication1/Menu_st_smen.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu_st_smen.xaml
+    /// </summary>
+    public partial class Menu_st_smen : Window
+    {
+        public Menu_st_smen()
+        {
+            InitializeComponent();
+        }
+
+        private void На_главную_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow form = new MainWindow();
+            form.Show();
+            this.Close();
+        }
+
+        private void Внести_новое_событие_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Редактировать_событие_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Редактировать_профиль_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
