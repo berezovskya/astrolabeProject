5e384f3d6da7f6880ae64f03771728c38ed049ab
SiteManager <sitemanager2222@outlook.com>
2018-03-02 19:13:25 +0500

    ффывы

---
+++ Firewatch/WpfApplication1/10ДанныеДляЗапроса.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для _10ДанныеДляЗапроса.xaml
+    /// </summary>
+    public partial class _10ДанныеДляЗапроса : Window
+    {
+        public _10ДанныеДляЗапроса()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Firewatch/WpfApplication1/9успешное сохранение.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для _9успешное_сохранение.xaml
+    /// </summary>
+    public partial class _9успешное_сохранение : Window
+    {
+        public _9успешное_сохранение()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
